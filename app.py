4397555850e648df4057eccd1582f54948258b9d
shahbaz <shahbazzulfiqar894@gmail.com>
2024-10-12 09:50:27 +0500

    update code and spaces in app file

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-def addition(a,b):
-    
-    return a +b
+def addition(a, b):
+    return a + b
 
 
-
-def mul(c,d):
-    return c *d
-
+def mul(c, d):
+    return c * d
 
 
-res=addition(12,22)
-res1=mul(10,20)
+res=addition(12, 22)
 
-print(res,res1)
+
+res1=mul(10, 20)
+
+print(res, res1)
