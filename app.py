fba247d8e4f42b8aab98635a0f2b203b606042c0
shahbaz <shahbazzulfiqar894@gmail.com>
2024-10-13 22:14:01 +0500

    update

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ def addition(a, b):
 def mul(c, d):
     return c * d
 
+
 def division(a, b):
     return a / b
 
-def subtraction(a , b):
+
+def subtract(a, b):
     return a - b
 
 
@@ -16,8 +18,8 @@ res = addition(12, 22)
 
 res1 = mul(10, 20)
 
-res2=subtraction(100, 44)
+res2 = subtract(100, 44)
 
-res3=division(72, 9)
+res3 = division(72, 9)
 
 print(res, res1, res2, res3)
