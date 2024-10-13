7b0e1b81a59fbee34f74f3c9c094b3fe4f2a7291
shahbaz <shahbazzulfiqar894@gmail.com>
2024-10-13 22:10:14 +0500

    add some more function

--- app.py
+++ app.py
@@ -5,9 +5,19 @@ def addition(a, b):
 def mul(c, d):
     return c * d
 
+def division(a, b):
+    return a / b
+
+def subtraction(a , b):
+    return a - b
+
 
 res = addition(12, 22)
 
 res1 = mul(10, 20)
 
-print(res, res1)
+res2=subtraction(100, 44)
+
+res3=division(72, 9)
+
+print(res, res1, res2, res3)
